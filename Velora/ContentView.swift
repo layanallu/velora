366a1b982a4d2931c86fb3dd1fb9a7aac8c01b17
layan <layan.allhidaan@gmail.com>
2026-02-22 00:50:40 +0300

    Initial Commit

---
+++ Velora/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Velora
+//
+//  Created by Layan on 05/09/1447 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Velora/VeloraApp.swift
@@ -0,0 +1,17 @@
+//
+//  VeloraApp.swift
+//  Velora
+//
+//  Created by Layan on 05/09/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct VeloraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
